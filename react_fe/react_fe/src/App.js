eeeb31279c58b29155e7516915f45fc74233eafb
김지현 <ledi000@naver.com>
2020-09-22 21:36:33 +0900

    Add top navigation bar

--- react_fe/react_fe/src/App.js
+++ react_fe/react_fe/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Home from "./view/Home";
+import Nav from "./view/Nav";
 
 function App() {
   return (
     <div>
+      <Nav />
       <Home />
     </div>
   );
