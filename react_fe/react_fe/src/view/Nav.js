f7ee8a42d5ea07c6ae06065d1e2ee9dd0d2dce82
김지현 <ledi000@naver.com>
2020-09-25 22:25:43 +0900

    Add link for About.js in navigation bar

--- react_fe/react_fe/src/view/Nav.js
+++ react_fe/react_fe/src/view/Nav.js
@@ -1,19 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./nav.scss";
 
 const Nav = () => {
   return (
-    <ul>
-      <li>Home</li>
-      &nbsp;|&nbsp;
-      <li>랜덤 추천 메뉴</li>
-      &nbsp;|&nbsp;
-      <li>About</li>
-      &nbsp;|&nbsp;
-      <li>Menu1</li>
-      &nbsp;|&nbsp;
-      <li>Menu2</li>
-    </ul>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        &nbsp;|&nbsp;
+        <li>랜덤 추천 메뉴</li>
+        &nbsp;|&nbsp;
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        &nbsp;|&nbsp;
+        <li>Menu1</li>
+        &nbsp;|&nbsp;
+        <li>Menu2</li>
+      </ul>
+    </div>
   );
 };
 
