11dd92ec1c934ed542c0bd868047e8ee26fcabce
김지현 <ledi000@naver.com>
2020-09-25 22:24:48 +0900

    Add Router to App.js and index.js

--- react_fe/react_fe/src/App.js
+++ react_fe/react_fe/src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
 import Home from "./view/Home";
+import About from "./view/About";
 import Nav from "./view/Nav";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Nav />
-      <Home />
-    </div>
+    <Router>
+      <nav>
+        <Nav />
+      </nav>
+      <main>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </main>
+    </Router>
   );
 }
 
