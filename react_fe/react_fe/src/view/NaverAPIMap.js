9ebce3db01ac64932726eaccce69be5430bd8667
김지현 <ledi000@naver.com>
2020-09-30 22:16:46 +0900

    Add input for searching places

--- react_fe/react_fe/src/view/NaverAPIMap.js
+++ react_fe/react_fe/src/view/NaverAPIMap.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { RenderAfterNavermapsLoaded, NaverMap } from "react-naver-maps";
 import { usePosition } from "use-position";
 import "./scss/naverApiMap.scss";
+import axios from "axios";
 
 export const NaverAPIMap = (props) => {
   //   const navermaps = window.naver.maps;
@@ -10,11 +11,40 @@ export const NaverAPIMap = (props) => {
     watch,
     { enableHighAccuracy: true }
   );
-  console.log(latitude);
-  console.log(longitude);
-
+  const [searchInput, setSearchInput] = useState("");
+  const onInputChange = (e) => {
+    setSearchInput(e.target.value);
+  };
+  const searchPlace = (e) => {
+    e.preventDefault();
+    axios
+      .get(
+        "https://dapi.kakao.com/v2/local/search/address.json?query=" +
+          encodeURIComponent(searchInput),
+        {
+          headers: {
+            Authorization: process.env.REACT_APP_API_KEY,
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        if (err.response.status == 401)
+          alert("error"); //나중에 바꾸기
+        if (err.response.status == 400) 
+          alert("error"); //나중에 바꾸기
+      });
+  };
   return (
     <RenderAfterNavermapsLoaded clientId={"sbw4q2m6xe"}>
+      <div className={"searchPlace"}>
+        <form onSubmit={searchPlace}>
+          <input type="text" onChange={onInputChange} value={searchInput} />
+          <button>검색</button>
+        </form>
+      </div>
       <NaverMap
         id={"map"}
         mapDivId={"react-naver-map"} // default name
