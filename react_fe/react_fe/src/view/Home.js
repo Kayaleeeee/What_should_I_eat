7a3332cba22d7997197d423dd1969573151621c6
김지현 <ledi000@naver.com>
2020-09-30 16:17:30 +0900

    Fix CSS property

--- react_fe/react_fe/src/view/Home.js
+++ react_fe/react_fe/src/view/Home.js
@@ -1,14 +1,13 @@
 import React from "react";
 import "./scss/home.scss";
 import NaverApiMap from "./NaverAPIMap";
-import Nav from "./Nav";
 
 function Home() {
   return (
     <div className={"outline"}>
       <div className={"container"}>
         <h1>Map</h1>
-        <NaverApiMap></NaverApiMap>
+        <NaverApiMap />
         <div className={"map"}></div>
       </div>
     </div>
