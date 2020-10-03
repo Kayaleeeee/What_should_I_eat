cd6fecea899f6a3034f49044fe464c1cd77ac401
kayaleeeee <gayeon71057@gmail.com>
2020-10-03 14:33:45 +0900

    Add Kakao map with place search api

--- react_fe/react_fe/src/view/KakaoMap.js
+++ react_fe/react_fe/src/view/KakaoMap.js
@@ -2,33 +2,29 @@
 
 import React, { Component } from "react";
 import "./scss/map.scss";
+import "./scss/kakaoMap.scss";
 
 class KakaoMap extends Component {
-  constructor(props) {
-    super(props);
-    let test = this.props.test;
-    this.state = {
-      lat: this.props.latitude,
-      long: this.props.longitude,
-    };
-  }
+  state = {
+    keyword: " ",
+  };
 
-  map;
-  markers = [];
-  inflowindoes = [];
-
-  componentDidMount() {}
+  handleChange = (e) => {
+    this.setState({
+      keyword: e.target.value,
+    });
+  };
 
   render() {
     let map;
 
-    // let lat = this.lat;
-    // let long = this.props.longitude;
+    let markers = [];
+    let inflowindoes = [];
 
     const script = document.createElement("script");
     script.async = true;
     script.src =
-      "https://dapi.kakao.com/v2/maps/sdk.js?appkey=&libraries=services,clusterer,drawing&autoload=false";
+      "https://dapi.kakao.com/v2/maps/sdk.js?appkey=&libraries=services,clusterer,drawing,markers&autoload=false";
     document.head.appendChild(script);
 
     script.onload = () => {
@@ -37,8 +33,7 @@ class KakaoMap extends Component {
         let options = {
           center: new kakao.maps.LatLng(
             this.props.latitude,
-            this.props.longitude,
-            console.log(this.props.latitude, this.props.longitude)
+            this.props.longitude
           ),
 
           level: 4,
@@ -49,89 +44,250 @@ class KakaoMap extends Component {
 
     let geocoder = new kakao.maps.services.Geocoder();
 
+    let keyword = this.state.keyword;
     console.log(this.props.latitude);
     console.log("geocode", geocoder);
     console.log(geocoder.coords);
 
     console.log(geocoder.LatLng);
 
-    let test = this.props.test;
-    let lat = this.props.latitude;
-    let long = this.props.longitude;
+    // let test = this.props.test;
+    // let lat = this.props.latitude;
+    // let long = this.props.longitude;
 
-    // geocoder.getLat() = this.props.latitude,
+    var ps = new kakao.maps.services.Places();
 
-    var marker = new kakao.maps.Marker(); // 클릭한 위치를 표시할 마커입니다
-    let infowindow = new kakao.maps.InfoWindow({ zindex: 1 }); // 클릭한 위치에 대한 주소를 표시할 인포윈도우입니다
+    // 검색 결과 목록이나 마커를 클릭했을 때 장소명을 표출할 인포윈도우를 생성합니다
+    var infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
 
-    // 현재 지도 중심좌표로 주소를 검색해서 지도 좌측 상단에 표시합니다
-    searchAddrFromCoords(geocoder, displayCenterInfo);
+    // 키워드로 장소를 검색합니다
+    searchPlaces();
 
-    // 지도 좌측상단에 지도 중심좌표에 대한 주소정보를 표출하는 함수입니다
-    function displayCenterInfo(result, status) {
+    // 키워드 검색을 요청하는 함수입니다
+    function searchPlaces() {
+      if (!keyword.replace(/^\s+|\s+$/g, "")) {
+        // alert("키워드를 입력해주세요!");
+        return false;
+      }
+
+      // 장소검색 객체를 통해 키워드로 장소검색을 요청합니다
+      ps.keywordSearch(keyword, placesSearchCB);
+    }
+
+    // 장소검색이 완료됐을 때 호출되는 콜백함수 입니다
+    function placesSearchCB(data, status, pagination) {
       if (status === kakao.maps.services.Status.OK) {
-        var infoDiv = document.getElementById("centerAddr");
+        // 정상적으로 검색이 완료됐으면
+        // 검색 목록과 마커를 표출합니다
+        displayPlaces(data);
 
-        for (var i = 0; i < result.length; i++) {
-          // 행정동의 region_type 값은 'H' 이므로
-          if (result[i].region_type === "H") {
-            infoDiv.innerHTML = result[i].address_name;
-            break;
-          }
-        }
+        // 페이지 번호를 표출합니다
+        displayPagination(pagination);
+      } else if (status === kakao.maps.services.Status.ZERO_RESULT) {
+        // alert("검색 결과가 존재하지 않습니다.");
+        return;
+      } else if (status === kakao.maps.services.Status.ERROR) {
+        // alert("검색 결과 중 오류가 발생했습니다.");
+        return;
       }
     }
 
-    // // 지도를 클릭했을 때 클릭 위치 좌표에 대한 주소정보를 표시하도록 이벤트를 등록합니다
-    // kakao.maps.event.addListener(map, "click", function (mouseEvent) {
-    //   searchDetailAddrFromCoords(mouseEvent.latLng, function (result, status) {
-    //     if (status === kakao.maps.services.Status.OK) {
-    //       var detailAddr = !!result[0].road_address
-    //         ? "<div>도로명주소 : " +
-    //           result[0].road_address.address_name +
-    //           "</div>"
-    //         : "";
-    //       detailAddr +=
-    //         "<div>지번 주소 : " + result[0].address.address_name + "</div>";
+    // 검색 결과 목록과 마커를 표출하는 함수입니다
+    function displayPlaces(places) {
+      var listEl = document.getElementById("placesList"),
+        menuEl = document.getElementById("menu_wrap"),
+        fragment = document.createDocumentFragment(),
+        bounds = new kakao.maps.LatLngBounds(),
+        listStr = "";
 
-    //       var content =
-    //         '<div class="bAddr">' +
-    //         '<span class="title">법정동 주소정보</span>' +
-    //         detailAddr +
-    //         "</div>";
+      // 검색 결과 목록에 추가된 항목들을 제거
+      removeAllChildNods(listEl);
 
-    //       // 마커를 클릭한 위치에 표시합니다
-    //       marker.setPosition(mouseEvent.latLng);
-    //       marker.setMap(map);
+      // 지도에 표시되고 있는 마커를 제거
+      removeMarker();
 
-    //       // 인포윈도우에 클릭한 위치에 대한 법정동 상세 주소정보를 표시합니다
-    //       infowindow.setContent(content);
-    //       infowindow.open(map, marker);
-    //     }
-    //   });
-    // });
+      for (var i = 0; i < places.length; i++) {
+        // 마커를 생성하고 지도에 표시
+        var placePosition = new kakao.maps.LatLng(places[i].y, places[i].x),
+          marker = addMarker(placePosition, i),
+          itemEl = getListItem(i, places[i]); // 검색 결과 항목 Element를 생성
 
-    // // 중심 좌표나 확대 수준이 변경됐을 때 지도 중심 좌표에 대한 주소 정보를 표시하도록 이벤트를 등록합니다
-    // kakao.maps.event.addListener(map, "idle", function () {
-    //   searchAddrFromCoords(map.getCenter(), displayCenterInfo);
-    // });
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+        // LatLngBounds 객체에 좌표를 추가
+        bounds.extend(placePosition);
 
-    function searchAddrFromCoords(coords, callback) {
-      // 좌표로 행정동 주소 정보를 요청합니다
+        // 마커와 검색결과 항목에 mouseover 했을때
+        // 해당 장소에 인포윈도우에 장소명을 표시
+        // mouseout 했을 때는 인포윈도우를 닫음
+        (function (marker, title) {
+          kakao.maps.event.addListener(marker, "mouseover", function () {
+            displayInfowindow(marker, title);
+          });
 
-      geocoder.coord2RegionCode(lat, long, callback);
+          kakao.maps.event.addListener(marker, "mouseout", function () {
+            infowindow.close();
+          });
+
+          itemEl.onmouseover = function () {
+            displayInfowindow(marker, title);
+          };
+
+          itemEl.onmouseout = function () {
+            infowindow.close();
+          };
+        })(marker, places[i].place_name);
+
+        fragment.appendChild(itemEl);
+      }
+
+      // 검색결과 항목들을 검색결과 목록 Elemnet에 추가
+      listEl.appendChild(fragment);
+      menuEl.scrollTop = 0;
+
+      // 검색된 장소 위치를 기준으로 지도 범위를 재설정
+      map.setBounds(bounds);
     }
 
-    function searchDetailAddrFromCoords(coords, callback) {
-      // 좌표로 법정동 상세 주소 정보를 요청합니다
-      geocoder.coord2Address(lat, long, callback);
+    // 검색결과 항목을 Element로 반환하는 함수
+    function getListItem(index, places) {
+      var el = document.createElement("li"),
+        itemStr =
+          '<span class="markerbg marker_' +
+          (index + 1) +
+          '"></span>' +
+          '<div class="info">' +
+          "   <h5>" +
+          places.place_name +
+          "</h5>";
+
+      if (places.road_address_name) {
+        itemStr +=
+          "    <span>" +
+          places.road_address_name +
+          "</span>" +
+          '   <span class="jibun gray">' +
+          places.address_name +
+          "</span>";
+      } else {
+        itemStr += "    <span>" + places.address_name + "</span>";
+      }
+
+      itemStr += '  <span class="tel">' + places.phone + "</span>" + "</div>";
+
+      el.innerHTML = itemStr;
+      el.className = "item";
+
+      return el;
     }
 
-    console.log(test, lat, long);
+    // 마커를 생성하고 지도 위에 마커를 표시하는 함수
+    function addMarker(position, idx, title) {
+      var imageSrc =
+          "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_number_blue.png", // 마커 이미지 url, 스프라이트 이미지를 씁니다
+        imageSize = new kakao.maps.Size(36, 37), // 마커 이미지의 크기
+        imgOptions = {
+          spriteSize: new kakao.maps.Size(36, 691), // 스프라이트 이미지의 크기
+          spriteOrigin: new kakao.maps.Point(0, idx * 46 + 10), // 스프라이트 이미지 중 사용할 영역의 좌상단 좌표
+          offset: new kakao.maps.Point(13, 37), // 마커 좌표에 일치시킬 이미지 내에서의 좌표
+        },
+        markerImage = new kakao.maps.MarkerImage(
+          imageSrc,
+          imageSize,
+          imgOptions
+        ),
+        marker = new kakao.maps.Marker({
+          position: position, // 마커의 위치
+          image: markerImage,
+        });
+
+      marker.setMap(map); // 지도 위에 마커를 표출
+      markers.push(marker); // 배열에 생성된 마커를 추가
+
+      return marker;
+    }
+
+    // 지도 위에 표시되고 있는 마커를 모두 제거
+    function removeMarker() {
+      for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+      }
+      markers = [];
+    }
+
+    // 검색결과 목록 하단에 페이지번호를 표시는 함수입니다
+    function displayPagination(pagination) {
+      var paginationEl = document.getElementById("pagination"),
+        fragment = document.createDocumentFragment(),
+        i;
+
+      // 기존에 추가된 페이지번호를 삭제합니다
+      while (paginationEl.hasChildNodes()) {
+        paginationEl.removeChild(paginationEl.lastChild);
+      }
+
+      for (i = 1; i <= pagination.last; i++) {
+        var el = document.createElement("a");
+        el.href = "#";
+        el.innerHTML = i;
+
+        if (i === pagination.current) {
+          el.className = "on";
+        } else {
+          el.onclick = (function (i) {
+            return function () {
+              pagination.gotoPage(i);
+            };
+          })(i);
+        }
+
+        fragment.appendChild(el);
+      }
+      paginationEl.appendChild(fragment);
+    }
+
+    // 검색결과 목록 또는 마커를 클릭했을 때 호출되는 함수
+    // 인포윈도우에 장소명을 표시
+    function displayInfowindow(marker, title) {
+      var content = '<div style="padding:5px;z-index:1;">' + title + "</div>";
+
+      infowindow.setContent(content);
+      infowindow.open(map, marker);
+    }
+
+    // 검색결과 목록의 자식 Element를 제거
+    function removeAllChildNods(el) {
+      while (el.hasChildNodes()) {
+        el.removeChild(el.lastChild);
+      }
+    }
+
+    // let lat = this.lat;
+    // let long = this.props.longitude;
 
     return (
-      <div className="kakaoMap">
+      <div class="map_wrap">
         <div id="map"></div>
+
+        <div id="menu_wrap" class="bg_white">
+          <div class="option">
+            <div>
+              <form onsubmit="searchPlaces(); return false;">
+                장소 검색 :{" "}
+                <input
+                  type="text"
+                  id="keyword"
+                  size="15"
+                  placeholder="검색할 장소 입력"
+                  onChange={this.handleChange}
+                />
+                <button type="submit">검색하기</button>
+              </form>
+            </div>
+          </div>
+          <hr />
+          <ul id="placesList"></ul>
+          <div id="pagination"></div>
+        </div>
       </div>
     );
   }
