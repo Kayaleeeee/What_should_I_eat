7bf4c4ba34ba9d608b2485630eadb59963920cbf
김지현 <ledi000@naver.com>
2020-11-14 04:45:00 +0900

    Add random menu food icon

--- react_fe/react_fe/src/container/Random.js
+++ react_fe/react_fe/src/container/Random.js
@@ -2,19 +2,100 @@ import React, { useState, useEffect } from "react";
 
 const Random = ({randomMenu, closeModal}) => {
   const [loading, setLoading] = useState(true);
+  const [randomMenuIcon, setRandomMenuIcon] = useState("🍴");
   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
     }, 1500);
+    setRandomMenuIcon(()=> {
+      if (randomMenu.includes("면") || randomMenu.includes("우동")) {
+        return "🍜";
+      }
+      else if (randomMenu.includes("한식") || randomMenu.includes("밥")) {
+        return "🍚";
+      }
+      else if (randomMenu.includes("국") || randomMenu.includes("탕")) {
+        return "🍲";
+      }
+      else if (randomMenu.includes("육류") || randomMenu.includes("고기")) {
+        return "🥓";
+      }
+      else if (randomMenu.includes("도시락")) {
+        return "🍱";
+      }
+      else if (randomMenu.includes("일식") || randomMenu.includes("초밥")) {
+        return "🍣";
+      }
+      else if (randomMenu.includes("카레")) {
+        return "🍛";
+      }
+      else if (randomMenu.includes("꼬치")) {
+        return "🍢";
+      }
+      else if (randomMenu.includes("다방")) {
+        return "☕️";
+      }
+      else if (randomMenu.includes("칵테일")) {
+        return "🍸";
+      }
+      else if (randomMenu.includes("와인")) {
+        return "🍷";
+      }
+      else if (randomMenu.includes("해물") || randomMenu.includes("생선")) {
+        return "🦐";
+      }
+      else if (randomMenu.includes("패스트푸드") || randomMenu.includes("햄버거")) {
+        return "🍔";
+      }
+      else if (randomMenu.includes("카페") || randomMenu.includes("디저트")) {
+        return "🍰";
+      }
+      else if (randomMenu.includes("제과") || randomMenu.includes("베이커리") || randomMenu.includes("빵")) {
+        return "🥐";
+      }
+      else if (randomMenu.includes("샌드위치")) {
+        return "🥪";
+      }
+      else if (randomMenu.includes("피자")) {
+        return "🍕";
+      }
+      else if (randomMenu.includes("치킨") || randomMenu.includes("닭")) {
+        return "🍗";
+      }
+      else if (randomMenu.includes("주점") || randomMenu.includes("술")) {
+        return "🍶";
+      }
+      else if (randomMenu.includes("맥주") || randomMenu.includes("호프")) {
+        return "🍺";
+      }
+      else if (randomMenu.includes("스테이크") || randomMenu.includes("립")) {
+        return "🥩";
+      }
+      else if (randomMenu.includes("양식") || randomMenu.includes("파스타") || randomMenu.includes("스파게티")) {
+        return "🍝";
+      }
+      else if (randomMenu.includes("찌개") || randomMenu.includes("전골")) {
+        return "🥘";
+      }
+      else if (randomMenu.includes("튀김") || randomMenu.includes("분식")) {
+        return "🍤";
+      }
+      else if (randomMenu.includes("멕시칸") || randomMenu.includes("브라질") || randomMenu.includes("타코")) {
+        return "🌮";
+      }
+      else {
+        return "🍴";
+      }
+    })
   }, [randomMenu]);
   return (
     <div className="random_container">
-        <h1>오늘의 추천 메뉴는?</h1>
+        <h1>😋오늘의 추천 메뉴는?🧐</h1>
         <div className="randomMenu">
           {loading ? (
             <img src={require("../static/food.gif")} alt="food image" />
           ) : (
-            randomMenu
+            randomMenuIcon + randomMenu + randomMenuIcon
           )}
           {!loading && (
             <button onClick={closeModal}>내 주변 식당 추천받기</button>
